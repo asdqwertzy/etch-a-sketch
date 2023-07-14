6290155662538e698122665619004250c3d4cf4f
asdqwertzy <asdf666@mail.com>
2023-07-14 05:10:32 +0200

    Fix slight typo, 256 divs in for loop rather than 257

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var container = document.querySelector('.container')
 container.style.cssText = "grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);"
 var divs = []
 
-for (let i = 0; i <= 256; i++)
+for (let i = 1; i <= 256; i++)
 {
     var x = document.createElement("div")
     divs.push(x)
