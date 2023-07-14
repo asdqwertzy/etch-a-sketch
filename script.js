6060109833bc01c634d0bff741ef1bfbc43cb5ff
asdqwertzy <asdf666@mail.com>
2023-07-14 06:56:22 +0200

    Add event listeners to all divs: bg color: black on mouse over

--- script.js
+++ script.js
@@ -2,9 +2,15 @@ var container = document.querySelector('.container')
 container.style.cssText = "grid-template-columns: repeat(16, 1fr); grid-template-rows: repeat(16, 1fr);"
 var divs = []
 
-for (let i = 1; i <= 256; i++)
-{
+for (let i = 1; i <= 256; i++) {
     var x = document.createElement("div")
     divs.push(x)
+    x.classList.add("grid-item")
     container.appendChild(x)
 }
+
+divs.forEach(function (x) {
+    x.addEventListener('mouseover', function (e) {
+        x.style.backgroundColor = "black";
+    })
+})
